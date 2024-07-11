df8e8b0f3004d93354cb900c147b6a1dadbc546b
SergioSanchezIglesias <sergiosanigle98@gmail.com>
2024-07-11 19:17:15 +0200

    SearchBar HTML y CSS hecho. Falta responsive

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { MaterialModule } from '../../../shared/material.module';
+import * as dataJSON from '../../../../assets/json/response.json';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, ReactiveFormsModule, AsyncPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  searchTerm = new FormControl('');
+  dataTest = dataJSON;
+  filteredOptions!: Observable<string[]>;
+
+  constructor() { }
 
 }
